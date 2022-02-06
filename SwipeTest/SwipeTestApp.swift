3992f0e5735710791c145b329c415fc7e1ab208c
Code Palace <codepalace@fastmail.se>
2022-02-06 16:12:47 +0100

    Initial Commit

---
+++ SwipeTest/SwipeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeTestApp.swift
+//  SwipeTest
+//
+//  Created by Federico on 06/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwipeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
